ff8e558f8582dbbe0ca5a289f9d1a50884e33cf0
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-03-07 14:25:20 +0000

    Starting of logics

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,50 @@
-const Name: string = "World";
-console.log(`Hello ${Name}!`);
+import client from "./config/whatsapp.config";
+import qrcode from "qrcode-terminal";
+import handleMessage from "./controllers/message.controller";
+import {Chat, Message} from "whatsapp-web.js";
+import startAutomation from "./utils";
+
+const startServer = async () => {
+    try {
+        client.on("qr", (qr) => {
+            qrcode.generate(qr, {small: true});
+        });
+
+        client.on("ready", () => {
+            console.log("client is ready");
+            startAutomation()
+        });
+
+        client.on("message", async (message: Message) => {
+            const chat: Chat = await message.getChat();
+            if (chat.isGroup) {
+                return null;
+            }
+            if (message.body.length === 0) {
+                return null;
+            }
+            if (message.from === "status@broadcast") {
+                return null;
+            }
+            const response: any = await new Promise((resolve, reject) => {
+                handleMessage(message.body, (err: any, res: any) => {
+                    if (err) {
+                        reject(new Error(err));
+                    } else {
+                        resolve(res);
+                    }
+                });
+            });
+
+            message.reply(response);
+        });
+
+
+        //  client.on("
+        client.initialize();
+    } catch (err: any) {
+        console.log(`Failed to initialize the client: ${err.message}`);
+    }
+};
+
+startServer();
